8d87e8dcb077cb7dd5c78ce6aaebae10f251e7ff
Bryan05U <bryanalexis.carcamo@alumnos.ulagos.cl>
2024-08-19 18:40:15 -0400

    Subiendo nueva carpeta

---
+++ archivo.py
@@ -0,0 +1,24 @@
+class Persona():
+    # Atributos de Clase (Características compartidas por todos los objetos de la clase)
+    nombre = "Bryan"
+    apellido = "Apellido"
+    edad = 18
+
+    # Métodos (Comportamientos)
+    def hablar(self):
+        print(f"{self.nombre} está hablando sobre política")
+
+    def caminar(self):
+        print(f"{self.nombre} está caminando por las calles")
+
+# Creación de un objeto de la clase Persona
+persona1 = Persona()
+
+# Acceso a los atributos y métodos del objeto
+print(f"Nombre: {persona1.nombre}")
+print(f"Apellido: {persona1.apellido}")
+print(f"Edad: {persona1.edad} años")
+
+# Llamando a los métodos de la Clase
+persona1.hablar()
+persona1.caminar()
